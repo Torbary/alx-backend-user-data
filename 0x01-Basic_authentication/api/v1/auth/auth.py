26235b5b1b7e0687b84e9bbec7f74e774e66461d
Torbary <torbarey020@gmail.com>
2023-09-05 22:29:22 +0100

    modified

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,14 +8,39 @@ from flask import request
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: list) -> bool:
-        """placeholder method for authentication"""
+        """
+        Placeholder method for authentication requirement.
+
+        Args:
+            path (str): The path being accessed.
+            excluded_paths (list): List of paths to be excluded from
+            authentication.
+
+        Returns:
+            bool: Always returns False (authentication not implemented yet).
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """Placeholder method for getting the authorization headerself.
+        """
+        Placeholder method for getting the authorization header.
+
+        Args:
+            request (Flask request): The Flask request object.
+
+        Returns:
+            str: Always returns None (authorization not implemented yet).
         """
         return None
 
     def current_user(self, request=None):
-        """placeholder method for getting the current user"""
+        """
+        Placeholder method for getting the current user.
+
+        Args:
+            request (Flask request): The Flask request object.
+
+        Returns:
+            TypeVar('User'): Always returns None (user not implemented yet).
+        """
         return None
