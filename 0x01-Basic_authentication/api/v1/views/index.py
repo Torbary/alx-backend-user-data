e9d2630fd8d094bcdfe6e2740aa12734e5d3abc3
Torbary <torbarey020@gmail.com>
2023-09-05 18:52:06 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route("/unauthorized", method=["GET"], strict_slashes=False)
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized_endpoint():
     """GET /api/v1/unauthorized
        Raise a 401 error
