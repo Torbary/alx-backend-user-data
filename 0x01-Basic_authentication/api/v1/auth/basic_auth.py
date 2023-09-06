d17320104d671797736dda8ea3ac35083fbeb058
Torbary <torbarey020@gmail.com>
2023-09-06 12:28:42 +0100

    extract base64 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,22 @@
 """
 from typing import Tuple, TypeVar
 from .auth import Auth
+import re
+import base64
 
 
 class BasicAuth(Auth):
     """Basic authentication class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+
+        """Returns the Base64 part of the Authorization
+            header for a Basic Authentication:
+        """
+        if type(authorization_header) == str:
+            pattern = r"Basic (?P<token>.+)"
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group("token")
+            return None
