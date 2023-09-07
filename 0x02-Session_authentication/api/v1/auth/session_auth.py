49486ffcfdcc89677bbf2a63e98290a919ba41b1
Torbary <torbarey020@gmail.com>
2023-09-07 14:30:43 +0100

    UPdate sessionAuth by creating a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """Session authentication
 """
 from .auth import Auth
+from typing import List
+import uuid
 
 
 class SessionAuth(Auth):
     """session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """"create a session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        # Generate a session ID using uuid4
+        session_id = str(uuid.uuid4())
+
+        # Store the user_id in the dictionary using the session ID as the Key
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
