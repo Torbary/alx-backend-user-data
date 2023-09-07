c4077dd6d6a160453d4e5f862dcdaa3061131d65
Torbary <torbarey020@gmail.com>
2023-09-07 16:04:01 +0100

    pycodestyle fixed

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ from typing import List, TypeVar
 from flask import request
 import os
 
+
 class Auth:
     """Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
