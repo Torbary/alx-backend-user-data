95929e7386763555acfa72b5c7da3a0e570d31c2
Torbary <torbarey020@gmail.com>
2023-09-08 23:18:07 +0100

    pycodestyle fixed

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -33,8 +33,9 @@ def session_auth_login():
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
     return response, 200
 
+
 @app_views.route("/auth_session/logout", methods=["DELETE"],
-                strict_slashes=False)
+                 strict_slashes=False)
 def logout() -> Tuple[str, int]:
     """Delete /api/v1/auth_session/logout"""
     from api.v1.app import auth
