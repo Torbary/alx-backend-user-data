16adadc6e49184797983c275b1a5e38339da68fd
Torbary <torbarey020@gmail.com>
2023-09-09 18:01:48 +0100

    Add expiration to the sessionAuth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,8 +9,8 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1 .auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SesssionExpAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -25,7 +25,7 @@ if auth_type == 'basic_auth':
 if auth_type == "session_auth":
     auth = SessionAuth()
 if auth_type == "session_exp_auth":
-    auth = SesssionExpAuth()
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
